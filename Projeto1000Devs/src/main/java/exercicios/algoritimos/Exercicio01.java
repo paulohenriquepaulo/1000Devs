8202470b18cbf4e3e30c094b51bb899407009ea0
paulohenriquepaulo <p.henrique461@gmail.com>
2022-08-18 11:10:58 -0300

    Exercicio01

--- Projeto1000Devs/src/main/java/exercicios/algoritimos/Exercicio01.java
+++ Projeto1000Devs/src/main/java/exercicios/algoritimos/Exercicio01.java
@@ -1,5 +1,28 @@
 package exercicios.algoritimos;
 
-public class Exercicio01 {
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        DecimalFormat format = new DecimalFormat("##.##");
+
+        System.out.print("Quantos metros o terreno possui de frente: ");
+        double metrosFrente = entrada.nextDouble();
+
+        System.out.print("Quantos metros o terreno possui de lateral: ");
+        double metrosLateral = entrada.nextDouble();
+
+        System.out.print("Informe o valor do metro quadrado: ");
+        double valorMetroQuadrado = entrada.nextDouble();
+
+        double areaTerreno = metrosFrente * metrosLateral;
+        double valorTerreno = valorMetroQuadrado * areaTerreno;
+
+        System.out.println("\nArea total do terreno de " + metrosFrente + " mts de frente com " +
+                metrosLateral + " mts de laeral é: " + areaTerreno + " mts" +
+                "\nO valor deste terreno é: " + format.format(valorTerreno));
+    }
 }
