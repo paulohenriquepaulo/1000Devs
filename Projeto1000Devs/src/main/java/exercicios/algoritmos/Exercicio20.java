9cf3a718e264a218b528c2044b19ebce6ad4cecc
paulohenriquepaulo <p.henrique461@gmail.com>
2022-08-19 12:52:25 -0300

    Exercicio20

---
+++ Projeto1000Devs/src/main/java/exercicios/algoritmos/Exercicio20.java
@@ -0,0 +1,22 @@
+package exercicios.algoritmos;
+
+import java.util.Scanner;
+
+public class Exercicio20 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um numero de 4 digitos: ");
+        Integer numero = entrada.nextInt();
+
+        String number = String.valueOf(numero);
+        char[] digitos = number.toCharArray();
+
+        System.out.println("\nMilhares: " + digitos[0] +
+                "\nCentenas: " + digitos[1] +
+                "\nDezenas:  " + digitos[2] +
+                "\nunidades: " + digitos[3]);
+
+    }
+}
