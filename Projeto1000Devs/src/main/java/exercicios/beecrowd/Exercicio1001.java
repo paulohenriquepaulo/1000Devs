97ec9d28910650c5d0aa0a46c27a5916ff77f40b
paulohenriquepaulo <p.henrique461@gmail.com>
2022-08-22 19:56:08 -0300

    Exercicio_Beecrowd 1001

---
+++ Projeto1000Devs/src/main/java/exercicios/beecrowd/Exercicio1001.java
@@ -0,0 +1,15 @@
+package exercicios.beecrowd;
+
+import java.util.Scanner;
+
+public class Exercicio1001 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int a = entrada.nextInt();
+        int b = entrada.nextInt();
+        int x = a + b;
+
+        System.out.println("X = " + x);
+    }
+}
