62dad4dc6da1ac547b3012f57d12980ff404278d
paulohenriquepaulo <p.henrique461@gmail.com>
2022-08-25 21:39:52 -0300

    Exercicio1016

---
+++ Projeto1000Devs/src/main/java/exercicios/beecrowd/Exercicio1016.java
@@ -0,0 +1,16 @@
+package exercicios.beecrowd;
+
+
+import java.util.Scanner;
+
+public class Exercicio1016 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int diastancia = entrada.nextInt() * 2;
+
+        System.out.println(diastancia + " minutos");
+    }
+}
